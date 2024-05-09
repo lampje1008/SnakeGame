d436917ff35462db09b882d024617b315424e8c3
lampje1008 <165754583+lampje1008@users.noreply.github.com>
2024-05-09 12:59:25 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,50 +1,151 @@
-#define START_X 8
-#define START_Y 8
-
 #include <curses.h>
-#include "./map.cpp"
-#include "./Snake.cpp"
+#include "map.cpp"
+#include "Snake.cpp"
+#include "Item.cpp"
+
+void printBlockAt(int x, int y, int colorPair) {
+	move(x, y);
+	attron(COLOR_PAIR(colorPair));
+	addch(ACS_BLOCK);
+	addch(ACS_BLOCK);
+	attron(COLOR_PAIR(colorPair));
+}
 
 int main() {
 	initscr();
+
 	start_color();
 	init_pair(1, COLOR_BLUE, COLOR_BLUE);
 	init_pair(2, COLOR_WHITE, COLOR_WHITE);
 	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(4, COLOR_BLACK, COLOR_WHITE);
 	init_pair(5, COLOR_YELLOW, COLOR_YELLOW);
-	MAP snakeMap;
-	snakeMap.draw_map();
-	snakeMap.draw_title();
-
-
+	MAP map;
+	map.draw_map();
+	map.draw_title();
 	Snake snake(3, RIGHT, MAP_SIZE / 2, MAP_SIZE / 2, 3, 10);
-	
 	while (true) {
-		for (int i = 0; ; ++i) {
-			if (snake.getPositionOf(i)[0] == -1 ||
-				snake.getPositionOf(i)[1] == -1)
+
+		// Snake Visualization
+		for (int index = 0; ; ++index) {
+			if (snake.getPositionOf(index)[0] == -1 ||
+				snake.getPositionOf(index)[1] == -1)
 				break;
 
-			move(START_X + snake.getPositionOf(i)[0],
-				 START_Y + snake.getPositionOf(i)[1]);
-			attron(COLOR_PAIR(5));
-			addch(ACS_BLOCK);
-			addch(ACS_BLOCK);
-			attron(COLOR_PAIR(5));
+			printBlockAt(START_X + snake.getPositionOf(index)[0],
+				START_Y + snake.getPositionOf(index)[1],
+				5);
 		}
 		refresh();
 
+		// Snake Update
 		int keyboardInput = getch();
-		switch (keyboardInput)
-		{
+		switch (keyboardInput) {
 		case KEY_UP:    snake.setDirectionTo(UP);    break;
 		case KEY_DOWN:  snake.setDirectionTo(DOWN);  break;
 		case KEY_RIGHT: snake.setDirectionTo(RIGHT); break;
 		case KEY_LEFT:  snake.setDirectionTo(LEFT);  break;
 		}
 		snake.move();
+
+
+
+	/*
+	bool victory = false;
+
+	// Curses Initialization
+	initscr();
+
+	start_color();
+	init_pair(1, COLOR_BLUE, COLOR_BLUE);
+	init_pair(2, COLOR_WHITE, COLOR_WHITE);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_BLACK, COLOR_WHITE);
+	init_pair(5, COLOR_YELLOW, COLOR_YELLOW);
+
+	// Map Initialization
+	MAP map;
+	map.draw_map();
+	map.draw_title();
+
+	// Snake Initialization
+	Snake snake(3, RIGHT, MAP_SIZE / 2, MAP_SIZE / 2, 3, 10);
+
+	// Item Initialization
+	GrothItem growthItem;
+	PoisonItem poisonItem;
+	
+	// Game Process
+	while (true) {
+
+		// Snake Visualization
+		for (int index = 0; ; ++index) {
+			if (snake.getPositionOf(index)[0] == -1 ||
+				snake.getPositionOf(index)[1] == -1)
+				break;
+
+			printBlockAt(START_X + snake.getPositionOf(index)[0],
+				START_Y + snake.getPositionOf(index)[1],
+				5);
+		}
+
+		// Item Visualization
+		printBlockAt(START_X + growthItem.x,
+			START_Y + growthItem.y,
+			1);
+
+		printBlockAt(START_X + poisonItem.x,
+			START_Y + poisonItem.y,
+			2);
+
+		refresh();
+
+		// Snake Update
+		int keyboardInput = getch();
+		switch (keyboardInput) {
+		case KEY_UP:    snake.setDirectionTo(UP);    break;
+		case KEY_DOWN:  snake.setDirectionTo(DOWN);  break;
+		case KEY_RIGHT: snake.setDirectionTo(RIGHT); break;
+		case KEY_LEFT:  snake.setDirectionTo(LEFT);  break;
+		}
+		snake.move();
+
+		// collide against walls
+		if (snake.getPositionOf()[0] == 0 ||
+			snake.getPositionOf()[0] == MAP_SIZE - 1 ||
+			snake.getPositionOf()[1] == 0 ||
+			snake.getPositionOf()[1] == MAP_SIZE - 1) {
+			// Game Over
+		}
+
+		// collide against its own body
+		for (int index = 1; ; ++index) {
+			if (snake.getPositionOf(index)[0] == -1 ||
+				snake.getPositionOf(index)[1] == -1)
+				break;
+
+			if (snake.getPositionOf() == snake.getPositionOf(index)) {
+				// Game Over
+			}
+		}
+
+		// get growth item
+		if (snake.getPositionOf()[0] == growthItem.x &&
+			snake.getPositionOf()[1] == growthItem.y) {
+			snake.increaseLength();
+			growthItem.onCollision();
+			growthItem.respawn();
+		}
+
+
+
+
 	}
+
+	if (victory) {
+
+	}
+	*/
 
 
 	endwin();
