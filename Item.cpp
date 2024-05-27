39a20d3ff1ac8bb84cbdbb3830e456525a59ef21
lampje1008 <165754583+lampje1008@users.noreply.github.com>
2024-05-27 22:21:10 +0900

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,24 +1,19 @@
 #include "Constants.h"
-
+#include <random>
+#include <chrono>
 using namespace std;
 
 class Item {
 public:
     int x;
     int y;
+    bool active;
+    int Item_type;//아이템 종류. 0이면 growth, 1이면 poison
 
     Item() {
         x = 0;
         y = 0;
+        active = false;
+        Item_type = std::rand() % 2;
     }
-};
-
-class GrowthItem : public Item {
-public:
-    GrowthItem() : Item() { }
-};
-
-class PoisonItem : public Item {
-public:
-    PoisonItem() : Item() { }
 };
